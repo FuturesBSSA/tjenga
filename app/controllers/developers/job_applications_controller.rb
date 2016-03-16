fcbdad39697e292e851113ecdbaa931b0af20353
Matthijs Kolkman <matthijs.h.j@Live.nl>
2016-03-16 11:23:50 +0100

    added functions to job applications

--- app/controllers/developers/job_applications_controller.rb
+++ app/controllers/developers/job_applications_controller.rb
@@ -1,17 +1,32 @@
 class Developers::JobApplicationsController < ApplicationController
-  def index
+  before_action :set_job, only: [:show]
 
+  def index
+    @job_applications = Job.all
   end
 
   def show
-
+    @fullname = Client.find(@job_application.client_id).first_name + ' ' + Client.find(@job_application.client_id).last_name
   end
 
   def new
-
+    @job_applications = Job.new
   end
 
   def create
+    @job_application = Job.find(params[:id])
+    @job_application.save
 
+    if @job_application.save
+      redirect_to
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_job_application
+    @job_application = Job.find(params[:id])
   end
 end
