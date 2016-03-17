96999a67e586c04565a8ea30aa372e712c10d703
Melchior Rutte <melchior.rutte@gmail.com>
2016-03-17 16:55:06 +0100

    now possible to apply for a job

--- app/controllers/developer/applications_controller.rb
+++ app/controllers/developer/applications_controller.rb
@@ -1,25 +1,26 @@
 class Developer::ApplicationsController < Developer::BaseController
-  before_action :set_job, only: [:show]
+  before_action :set_job, only: [:new, :create]
 
   def index
     @applications = current_developer.applications.all
-    @developer = current_developer
   end
 
   def show
     # @fullname = Client.find(@application.client_id).first_name + ' ' + Client.find(@application.client_id).last_name
+    @application = Application.find(params[:id])
   end
 
   def new
-    @applications = Job.new
+    @applications = @job.applications.new
+    @developer = current_developer
   end
 
   def create
-    @application = Job.find(params[:id])
-    @application.save
+    @application = @job.applications.new(application_param)
+    @application.developer = current_developer
 
     if @application.save
-      redirect_to
+      redirect_to developer_application_path(@application)
     else
       render :new
     end
@@ -27,7 +28,11 @@ class Developer::ApplicationsController < Developer::BaseController
 
   private
 
+  def application_param
+    params.require(:application).permit(:motivation, :price)
+  end
+
   def set_job
-    @job = Job.find(params[:id])
+    @job = Job.find(params[:job_id])
   end
 end
