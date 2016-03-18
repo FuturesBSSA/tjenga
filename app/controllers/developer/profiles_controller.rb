0960b0d4bb8e0dfa95f6fb344a3ffc061c99ee1f
Melchior Rutte <melchior.rutte@gmail.com>
2016-03-18 15:59:15 +0100

    profile edit now possible for client and developer

--- app/controllers/developer/profiles_controller.rb
+++ app/controllers/developer/profiles_controller.rb
@@ -2,15 +2,21 @@ class Developer::ProfilesController < Developer::BaseController
   def show
     @developer = current_developer
     @applications = current_developer.applications
+    @fullname = current_developer.first_name + " " + current_developer.last_name
   end
 
   def edit
-    @developer = current_developer.find(params[:id])
+    @developer = current_developer
   end
 
   def update
-    @developer = params.require(:developer).permit(:email,:first_name, :last_name, :phone_number, :city, :address, :photo)
-    @developer.update!
+    @developer = current_developer
+    @developer.update!(find_params)
     redirect_to developer_profile_path
+  end
+
+  private
+  def find_params
+    @developer = params.require(:developer).permit(:email,:first_name, :last_name, :phone_number, :city, :address, :photo)
   end
 end
