72a379d18e0f84d55cc4d0ad9c34c8bf6eb8444d
Melchior Rutte <melchior.rutte@gmail.com>
2016-03-18 15:00:16 +0100

    animate navbar and fix profile controller for dev

--- app/controllers/developer/profiles_controller.rb
+++ app/controllers/developer/profiles_controller.rb
@@ -1,13 +1,16 @@
 class Developer::ProfilesController < Developer::BaseController
   def show
     @developer = current_developer
+    @applications = current_developer.applications
   end
 
   def edit
-
+    @developer = current_developer.find(params[:id])
   end
 
   def update
-
+    @developer = params.require(:developer).permit(:email,:first_name, :last_name, :phone_number, :city, :address, :photo)
+    @developer.update!
+    redirect_to developer_profile_path
   end
 end
