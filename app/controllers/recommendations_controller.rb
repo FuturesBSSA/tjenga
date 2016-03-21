b6e9abd0578432cef96a55cec6b95555f86bdcfd
Matthijs Kolkman <matthijs.h.j@Live.nl>
2016-03-21 11:18:28 +0100

    add basic recommendation input

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,2 +1,19 @@
 class RecommendationsController < ApplicationController
+  def new
+    @recommendation = Recommendation.new
+    @developer = Developer.find(params[:developer_id])
+  end
+
+  def create
+    @recommendation = Recommendation.new
+
+    @client = current_client
+    @developer = Developer.find(params[:developer_id])
+
+    if @recommendation.save
+      redirect_to developer_path(@developer)
+    else
+      render :new
+    end
+  end
 end
