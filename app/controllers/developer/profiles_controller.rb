9444beff06c293bf5f4ea1628d4159f84e6638ae
Melchior Rutte <melchior.rutte@gmail.com>
2016-03-21 16:43:08 +0100

    add some basic styling

--- app/controllers/developer/profiles_controller.rb
+++ app/controllers/developer/profiles_controller.rb
@@ -2,6 +2,7 @@ class Developer::ProfilesController < Developer::BaseController
   def show
     @developer = current_developer
     @applications = current_developer.applications
+    @reviews = @developer.job_reviews
     @fullname = current_developer.first_name + " " + current_developer.last_name
   end
 
