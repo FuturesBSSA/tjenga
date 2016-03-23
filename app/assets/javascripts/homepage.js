bf731c067ae364764fb7f05d5ac1113506b25d6b
Matthijs Kolkman <matthijs.h.j@Live.nl>
2016-03-24 00:08:39 +0100

    add javascrip (not fully working)

---
+++ app/assets/javascripts/homepage.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+        $("#client-info").click(function() {
+            $("#client").fadeToggle();
+        });
+
+        $("#developer-info").click(function() {
+            $("#developer").fadeToggle();
+        });
+    });
