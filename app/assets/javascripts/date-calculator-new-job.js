3599e77b14337160eec5add307306957828a5d41
Matthijs Kolkman <matthijs.h.j@Live.nl>
2016-03-25 23:58:03 +0100

    first tests

---
+++ app/assets/javascripts/date-calculator-new-job.js
@@ -0,0 +1,13 @@
+// $(document).ready(function() {
+  function calcMonth() {
+    var month_start = $('#job_start_date_2i').val();
+    var month_deadline = $('#job_deadline_2i').val();
+
+    var start = parseInt(month_start);
+    var deadline = parseInt(month_deadline);
+
+    $('p').html('Duration in months: ' + (deadline - start));
+    console.log(deadline - start);
+    $('select').change( calcMonth );
+  };
+// });
