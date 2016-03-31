410abf4e8ab521aefc609d03df8aabdef6798d34
Melchior Rutte <melchior.rutte@gmail.com>
2016-03-31 11:44:36 +0200

    add link to developer name in show application

--- app/controllers/client/applications_controller.rb
+++ app/controllers/client/applications_controller.rb
@@ -2,9 +2,11 @@ class Client::ApplicationsController < Client::BaseController
   before_action :find_application, except: (:index)
   def index
     @applications = current_client.applications
+    # @developer = Developer.find(@applications.developer_id)
   end
 
   def show
+    @developer = Developer.find(@application.developer_id)
   end
 
   def accept
