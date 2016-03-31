0e1e0ff1dcaee929c832398f84217c1541a6a321
Melchior Rutte <melchior.rutte@gmail.com>
2016-03-31 12:30:20 +0200

    add flash message when job is created

--- app/controllers/client/jobs_controller.rb
+++ app/controllers/client/jobs_controller.rb
@@ -17,8 +17,12 @@ class Client::JobsController < Client::BaseController
     @job = Job.new(job_param)
     @client = current_client
     @job.client = @client
-    @job.save!
-    redirect_to client_job_path(@job)
+    if @job.save!
+      flash[:success] = "You successfully created a job!"
+      redirect_to client_job_path(@job)
+    else
+      render 'new'
+    end
   end
 
   def edit
