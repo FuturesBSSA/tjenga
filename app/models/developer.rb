e9ed485b3375e2955ab8b294ac5f76ba37188102
Melchior Rutte <melchior.rutte@gmail.com>
2016-04-18 13:20:05 +0200

    prepare for adding confirmation

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,7 +1,7 @@
 class Developer < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:linkedin]
   after_create :send_welcome_email
