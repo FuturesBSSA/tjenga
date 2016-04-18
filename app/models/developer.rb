37a3769084dba5bea1b2fc8b83fe0e780be47caf
Melchior Rutte <melchior.rutte@gmail.com>
2016-04-18 13:41:12 +0200

    minor fix to client and dev model

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,7 +1,7 @@
 class Developer < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:linkedin]
   after_create :send_welcome_email
